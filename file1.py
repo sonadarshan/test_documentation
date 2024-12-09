02f8fc5cbe9d692b38b0a3961e251c112d5f88f2
Sonadarshan N G <Sonadarshan.NG@ivanti.com>
2024-12-09 11:27:21 +0530

    check wf

--- file1.py
+++ file1.py
@@ -150,3 +150,10 @@ def tan(angle: float) -> float:
         float: The tangent of the angle.
     """
     return math.tan(angle)
+
+def test():
+    """
+    Test add
+    :return:
+    """
+    pass
